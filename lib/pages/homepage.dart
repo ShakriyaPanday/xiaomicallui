c9863465a433b7d31d3a59c6bd515ae25e7fbf6f
Shakriya Panday <fenom.shakriyapanday@gmail.com>
2022-11-09 23:36:15 +0545

    User information decorated

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -14,17 +14,38 @@ class HomePage extends StatelessWidget {
               Center(
                 child: Column(
                   children: [
+                    SizedBox(
+                      height: 10,
+                    ),
                     Container(
                       decoration: BoxDecoration(
-                          color: Colors.green,
-                          borderRadius: BorderRadius.circular(30)),
-                      height: 30,
-                      width: 30,
-                      child: Icon(Icons.person),
+                          color: Colors.deepOrange[300],
+                          borderRadius: BorderRadius.circular(50)),
+                      height: 100,
+                      width: 100,
+                      child: Icon(
+                        Icons.person,
+                        color: Colors.white,
+                        size: 80,
+                      ),
                     ),
-                    Text("Shakriya Panday"),
-                    Text("9869117846"),
-                    Text("01:04"),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    Text(
+                      "Shakriya Panday",
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 35),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      "01:04",
+                      style: TextStyle(color: Colors.black, fontSize: 20),
+                    ),
                   ],
                 ),
               )
